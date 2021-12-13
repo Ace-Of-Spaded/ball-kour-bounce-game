259d6efc96b48d0bef687ccb6dfced4e13abc5d2
Ace-Of-Spaded <thomasdl@s.dcsdk12.org>
2021-12-13 16:27:45 -0700

    My game

---
+++ scripts/app.js
@@ -0,0 +1,496 @@
+// @ts-check
+/** @type {HTMLCanvasElement} */
+// @ts-ignore
+const canvas = document.getElementById("game-canvas");
+const ctx = canvas.getContext("2d");
+canvas.width = 800;
+canvas.height = 600;
+
+/** @param {number} pt */
+function parabollicEasing(pt) {
+	let x = pt * 4 - 2;
+	let y = x * x * -1 + 4;
+	return y / 4;
+}
+
+class KeyboardState {
+	constructor() {
+		this.isAccelerating = false;
+		this.isBraking = false;
+		this.registerEventHandlers();
+	}
+
+	registerEventHandlers() {
+		window.addEventListener("keydown", (e) => {
+			switch (e.key) {
+				case "a":
+				case "ArrowLeft":
+					this.isBraking = true;
+					break;
+				case "d":
+				case "ArrowRight":
+					this.isAccelerating = true;
+					break;
+			}
+		});
+
+		window.addEventListener("keyup", (e) => {
+			switch (e.key) {
+				case "a":
+				case "ArrowLeft":
+					this.isBraking = false;
+					break;
+				case "d":
+				case "ArrowRight":
+					this.isAccelerating = false;
+					break;
+			}
+		});
+	}
+}
+
+class Player {
+	/** @param {Array<SafePlatform>} [platforms] */
+	constructor(platforms) {
+		this.platforms = platforms;
+		this.maxBounceHeight = canvas.height / 2;
+		this.yOfLastBounce = 0;
+		this.x = canvas.width * 0.25;
+		this.y = 0;
+		this.prevY = 0;
+		this.color = 1;
+
+		this.bounceTime = 1000;
+		this.timeSinceLastBounce = 0;
+		this.radius = 16;
+
+		this.leftSide = this.x - this.radius / 2;
+		this.rightSide = this.x + this.radius / 2;
+	}
+
+	/** @param {number} elapsedTime */
+	update(elapsedTime) {
+		this.timeSinceLastBounce += elapsedTime;
+		const isMovingDown = this.timeSinceLastBounce > this.bounceTime / 2;
+
+		let ef = parabollicEasing(this.timeSinceLastBounce / this.bounceTime);
+		this.y = this.yOfLastBounce - ef * this.maxBounceHeight;
+		this.color++;
+
+		this.platforms.forEach((p) => {
+			let isInside =
+				this.rightSide >= p.x && this.leftSide <= p.x + p.width;
+			let isPlatformBelowMe =
+				isInside && (this.y < p.y || this.prevY < p.y);
+
+			if (
+				isMovingDown &&
+				isPlatformBelowMe &&
+				this.y + this.radius >= p.y
+			) {
+				this.timeSinceLastBounce = 0;
+				this.yOfLastBounce = p.y;
+
+				let event = new CustomEvent("bkb-bounce", { detail: p });
+				document.dispatchEvent(event);
+			}
+		});
+
+		this.prevY = this.y;
+	}
+
+	render() {
+		ctx.save();
+		ctx.fillStyle = `hsla(${this.color}, 100%, 50%, 1)`;
+		ctx.beginPath();
+		ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
+		ctx.fill();
+		ctx.restore();
+	}
+}
+
+class Game {
+	/** @param {KeyboardState} kb */
+	constructor(kb) {
+		this.kb = kb;
+		this.speed = 0;
+		this.maxSpeed = 100;
+		this.accelerationRate = 5;
+		this.accelerationInterval = 100;
+		this.timeSinceLastAcceleration = 0;
+
+		this.score = 0;
+		this.scoreX = canvas.width - 150;
+		this.scoreY = 95;
+
+		this.wireUpListeners();
+
+		this.bgImage = new Image();
+		this.bgImage.src = "/images/background.png";
+
+		// w / 600 = 2048 / 1152
+
+		this.imageHeight = 800;
+		this.imageWidth = 1400;
+		this.imageX = 0;
+	}
+
+	/** @param {number} elapsedTime  */
+	update(elapsedTime) {
+		this.timeSinceLastAcceleration += elapsedTime;
+
+		if (
+			this.kb.isAccelerating &&
+			this.speed < this.maxSpeed &&
+			this.timeSinceLastAcceleration >= this.accelerationInterval
+		) {
+			this.speed += this.accelerationRate;
+			this.timeSinceLastAcceleration = 0;
+		}
+
+		if (this.kb.isBraking) {
+			this.speed = 0;
+			this.timeSinceLastAcceleration = 0;
+		}
+
+		if (
+			!this.kb.isAccelerating &&
+			!this.kb.isBraking &&
+			this.timeSinceLastAcceleration >= this.accelerationInterval &&
+			this.speed > 0
+		) {
+			// decelarating
+			this.speed -= this.accelerationRate;
+			this.timeSinceLastAcceleration = 0;
+		}
+
+		// parallax by moving at 10% of speed
+		this.imageX -= this.speed * 0.1;
+		if (this.imageX + this.imageWidth <= 0) {
+			this.imageX = 0;
+		}
+	}
+
+	render() {
+		ctx.save();
+		ctx.drawImage(
+			this.bgImage,
+			this.imageX,
+			0,
+			this.imageWidth,
+			this.imageHeight
+		);
+
+		ctx.drawImage(
+			this.bgImage,
+			this.imageX + this.imageWidth,
+			0,
+			this.imageWidth,
+			this.imageHeight
+		);
+
+		ctx.restore();
+
+		ctx.save();
+		ctx.fillStyle = "pink";
+		ctx.strokeStyle = "purple";
+		ctx.font = "90px fantasy";
+
+		ctx.fillText(`${this.score}`, this.scoreX, this.scoreY);
+		ctx.strokeText(`${this.score}`, this.scoreX, this.scoreY);
+		ctx.restore();
+	}
+
+	wireUpListeners() {
+		document.addEventListener("bkb-bounce", (e) => {
+			// @ts-ignore
+			let p = e.detail;
+
+			if (p.isScorable && !p.isScored) {
+				this.score++;
+				p.isScored = true;
+			}
+		});
+	}
+}
+
+class Tracer {
+	/**
+	 * @param {Player} p
+	 * @param {Game} g
+	 */
+	constructor(p, g) {
+		this.p = p;
+		this.g = g;
+
+		this.x = p.x;
+		this.y = p.y;
+
+		this.isVisible = true;
+		this.opacity = 1;
+
+		this.fadeRate = 0.1;
+		this.fadeInterval = 100;
+		this.timeSinceFade = 0;
+		this.color = 1;
+	}
+
+	/**
+	 * @param {number} timeElapsed
+	 */
+	update(timeElapsed) {
+		this.timeSinceFade += timeElapsed;
+		this.x -= this.g.speed;
+		this.color += 12;
+
+		if (this.timeSinceFade >= this.fadeInterval) {
+			this.opacity -= this.fadeRate;
+			this.timeSinceFade = 0;
+		}
+
+		this.isVisible = this.opacity > 0;
+	}
+
+	render() {
+		ctx.save();
+
+		ctx.fillStyle = `hsla(${this.color}, 100%, 50%, ${this.opacity})`;
+		ctx.beginPath();
+		ctx.arc(this.x, this.y, this.p.radius / 2, 0, Math.PI * 2, true);
+		ctx.fill();
+
+		ctx.restore();
+	}
+}
+
+class movingPlatformY {
+    /**
+     * @param {Game} g
+     */
+    constructor(g) {
+        this.game = g;
+        this.width = 400;
+        this.height = 32;
+        this.x = 0;
+        this.bottemY = 550;
+        this.topY = 300;
+        this.y = this.topY;
+        this.speed = 3;
+        this.isMovingDown = false
+        this.isVisible = true;
+        this.color = 30;
+        this.isIncreasing = true;
+    }
+
+    /**
+     * @param {number} elapsedTime
+     */
+    // @ts-ignore
+    update(elapsedTime) {
+        this.x -= this.game.speed;
+        this.isVisible = this.x + this.width > 0;
+        
+        if(this.y <= this.topY)
+        {
+            this.isMovingDown = true
+        }
+        if (this.y >= this.bottemY)
+        {
+            this.isMovingDown = false
+        }
+        if (this.isMovingDown)
+        {
+            this.y += this.speed;
+        }
+        if(!this.isMovingDown)
+        {
+            this.y -= this.speed;
+        }
+
+        if(this.color >= 60)
+        {
+            this.isIncreasing = false
+        }
+        if(this.color <= 30)
+        {
+            this.isIncreasing = true
+        }
+        if (this.isIncreasing)
+        {
+            this.color += 0.5;
+        }
+        if (!this.isIncreasing)
+        {
+            this.color -= 0.5;
+        }
+       
+
+    }
+
+    render() {
+        ctx.save();
+        ctx.fillStyle = `hsla(${this.color}, 100%, 50%, 1)`;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+        ctx.restore();
+    }
+}
+
+class SafePlatform {
+	/**
+	 * @param {Game} g
+	 */
+	constructor(g) {
+		this.game = g;
+		this.width = 400;
+		this.height = 32;
+		this.color = 270;
+
+		this.x = 0;
+		this.y = canvas.height - this.height * 1.5;
+
+		this.isVisible = true;
+		this.isIncreasing = true;
+	}
+
+	/**
+	 * @param {number} elapsedTime
+	 */
+	update(elapsedTime) {
+		this.x -= this.game.speed;
+		this.isVisible = this.x + this.width > 0;
+
+		if (this.color > 300) {
+			this.isIncreasing = false;
+		}
+		if (this.color < 270) {
+			this.isIncreasing = true;
+		}
+		if (this.isIncreasing) {
+			this.color += 0.1;
+		}
+		if (!this.isIncreasing) {
+			this.color -= 0.1;
+		}
+	}
+
+	render() {
+		ctx.save();
+		ctx.fillStyle = `hsla(${this.color}, 100%, 55%, 1)`;
+		ctx.fillRect(this.x, this.y, this.width, this.height);
+		ctx.restore();
+	}
+}
+
+
+class ScorePlatform {
+	/**
+	 * @param {Game} g
+	 */
+	constructor(g) {
+		this.game = g;
+		this.width = 32;
+		this.height = canvas.height;
+
+		this.x = 0;
+		this.y = canvas.height - 100;
+
+		this.isVisible = true;
+
+		this.isScored = false;
+		this.isScorable = true;
+		this.scoreImage = new Image();
+		this.scoreImage.src = "/images/score.png";
+		this.scoreImageX = this.x;
+		this.scoreImageY = this.y;
+	}
+
+	/**
+	 * @param {number} elapsedTime
+	 */
+	update(elapsedTime) {
+		this.x -= this.game.speed;
+		this.isVisible = this.x + this.width > 0;
+		this.scoreImageX = this.x;
+		this.scoreImageY = this.y;
+	}
+
+	render() {
+		ctx.save();
+		ctx.fillRect(this.x, this.y, this.width, this.height);
+		ctx.restore();
+		ctx.save();
+		ctx.drawImage(this.scoreImage, this.scoreImageX, this.scoreImageY);
+		ctx.restore();
+	}
+}
+
+class PlatformManager {
+	constructor(platforms, game) {
+		this.platforms = platforms;
+		this.game = game;
+	}
+
+	update() {
+		let lastPlatform = platforms[platforms.length - 1];
+		let furthestX = lastPlatform.x + lastPlatform.width;
+
+		while (furthestX < canvas.width * 2) {
+			let spacer = Math.floor(Math.random() * 168 + 32);
+			let spacerY = Math.floor(Math.random() * 150 + 300);
+
+			let nextPlatformType = Math.random();
+
+			let p;
+
+			if (nextPlatformType > 0.1 && nextPlatformType < 0.2) {
+				p = new SafePlatform(this.game);
+			} else if (nextPlatformType > 0.2 && nextPlatformType < 0.5)
+			{
+				p = new ScorePlatform(this.game);
+			}
+			else
+			{
+				p= new movingPlatformY(this.game)
+			}
+
+			p.x = furthestX + spacer;
+			p.y = spacerY;
+			this.platforms.push(p);
+			furthestX += spacer + p.width;
+		}
+	}
+}
+
+let kb = new KeyboardState();
+let game = new Game(kb);
+
+let platforms = [new SafePlatform(game)];
+let pm = new PlatformManager(platforms, game);
+let player = new Player(platforms);
+let tracers = [new Tracer(player, game)];
+
+let currentTime = 0;
+
+/** @param {number} timestamp */
+function gameLoop(timestamp) {
+	let timeElapsed = timestamp - currentTime;
+	currentTime = timestamp;
+	// console.log(timeElapsed, timestamp);
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+	tracers.push(new Tracer(player, game));
+
+	pm.update();
+	let gameObjects = [game, ...tracers, player, ...platforms];
+
+	gameObjects.forEach((o) => {
+		o.update(timeElapsed);
+		o.render();
+	});
+
+	tracers = tracers.filter((t) => t.isVisible);
+
+	requestAnimationFrame(gameLoop);
+}
+
+requestAnimationFrame(gameLoop)
